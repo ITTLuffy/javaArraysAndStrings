b966dad7b0cf336db7d2d4841d043883b9cecb60
alessio.ferrari <alessio.ferrari@marconirovereto.it>
2024-11-29 16:11:33 +0100

    Fix exercise: branch

--- esercizioPerVerifica/src/App.java
+++ esercizioPerVerifica/src/App.java
@@ -9,39 +9,45 @@ public class App {
 
         String stringa = " ";
         // VERSIONE SENZA ARRAY
-        //
-        // int h = n;
-        //
-        // for (int i = 1; i < n; i += 2) {
-        // for (int j = h/2; j > 0; j--) {
-        // System.out.print(stringa); // stampo gli spazi
-        // }
-        //
-        // for (int j = 0; j < i; j++) {
-        // System.out.print("*"); // stampo gli asterischi
-        // }
-        // System.out.println("");
-        // h -= 2;
-        //
-        // }
 
-        // VERSIONE CON ARRAY
         int h = n;
-        char[] vettore = new char[n];
 
-        Arrays.fill(vettore, '*');
         for (int i = 1; i < n; i += 2) {
             for (int j = h / 2; j > 0; j--) {
                 System.out.print(stringa); // stampo gli spazi
             }
 
             for (int j = 0; j < i; j++) {
-                System.out.print(vettore[j]); // stampo gli asterischi
+                System.out.print("*"); // stampo gli asterischi
             }
             System.out.println("");
             h -= 2;
-
         }
+
+        System.out.println("        ***");
+        System.out.println("       *****");
+
+
+        // VERSIONE CON ARRAY
+        /*
+         * int h = n;
+         * char[] vettore = new char[n];
+         * 
+         * Arrays.fill(vettore, '*');
+         * for (int i = 1; i < n; i += 2) {
+         * for (int j = h / 2; j > 0; j--) {
+         * System.out.print(stringa); // stampo gli spazi
+         * }
+         * 
+         * for (int j = 0; j < i; j++) {
+         * System.out.print(vettore[j]); // stampo gli asterischi
+         * }
+         * System.out.println("");
+         * h -= 2;
+         * 
+         * }
+         * 
+         */
 
     }
 }
